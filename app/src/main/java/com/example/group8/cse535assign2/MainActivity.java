efc2c575640156a795f8bdee8b4afa884865ce72
chenyang Li <leonlcycat@gmail.com>
2018-08-02 14:36:21 -0700

    PartB
     upload the  DB to the sever

--- app/src/main/java/com/example/group8/cse535assign2/MainActivity.java
+++ app/src/main/java/com/example/group8/cse535assign2/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.group8.cse535assign2;
 
 
+import android.annotation.SuppressLint;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -21,8 +23,13 @@ import com.jjoe64.graphview.Viewport;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 
+import java.io.DataOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Random;
 
 import android.database.SQLException;
@@ -68,6 +75,14 @@ public class MainActivity extends AppCompatActivity {
     SQLiteDatabase db;
     //public static final String DATABASE_NAME = "patient";
     //public static final String DATABASE_LOCATION = Environment.getExternalStorageState()+ File.separator +"mydata" + File.separator + DATABASE_NAME;
+
+    /** Variables for upload* */
+    int serverResponseCode = 0;
+    ProgressDialog dialog = null;
+    String upLoadServerUri = "http://impact.asu.edu/CSE535Spring18Folder/UploadToServer.php";
+    String uploadFilePath =null;
+    String uploadFileName = null;
+
 
     @ Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -121,9 +136,6 @@ public class MainActivity extends AppCompatActivity {
         viewport.setScrollable(true);
 
         Button startButton = (Button) findViewById(R.id.start);
-
-
-
 
         /**
          * create a start button
@@ -206,7 +218,6 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
-
 
 
 
@@ -377,8 +388,168 @@ public class MainActivity extends AppCompatActivity {
 
         */
 
+        /**Create upload button**/
+        Button uploadButton;
+        uploadButton = (Button)findViewById(R.id.uploadDB);
 
+        uploadFilePath = this.getExternalFilesDir(null) + "/mydata";
+        uploadFileName = "/group8.db";
+        /**Php script**/
+        upLoadServerUri = "http://impact.asu.edu/CSE535Spring18Folder/UploadToServer.php";
 
+        uploadButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                dialog = ProgressDialog.show(MainActivity.this, "", "Uploading file...", true);
+
+                new Thread(new Runnable() {
+                    public void run() {
+
+                        uploadDB(uploadFilePath + "" + uploadFileName);
+
+                    }
+                }).start();
+            }
+        });
+
+    }
+
+    @SuppressLint("LongLogTag")
+    public void uploadDB(String sourceFileUri){
+        String fileName = sourceFileUri;
+        HttpURLConnection conn = null;
+        DataOutputStream dos = null;
+        String lineEnd = "\r\n";
+        String twoHyphens = "--";
+        String boundary = "*****";
+        int bytesRead, bytesAvailable, bufferSize;
+        byte[] buffer;
+        int maxBufferSize = 1 * 1024 * 1024;
+        File sourceFile = new File(sourceFileUri);
+
+        if (!sourceFile.isFile()) {
+
+            dialog.dismiss();
+
+            Log.e("uploadFile", "Source File not exist :"
+                    +uploadFilePath + "" + uploadFileName);
+
+            runOnUiThread(new Runnable() {
+                public void run() {
+                    Toast.makeText(MainActivity.this, "Source File not exist :", Toast.LENGTH_LONG).show();
+                }
+            });
+
+            return;
+        }
+        else
+        {
+            try {
+
+                // open a URL connection to the Servlet
+                FileInputStream fileInputStream = new FileInputStream(sourceFile);
+                URL url = new URL(upLoadServerUri);
+
+                // Open a HTTP  connection to  the URL
+                conn = (HttpURLConnection) url.openConnection();
+                conn.setDoInput(true); // Allow Inputs
+                conn.setDoOutput(true); // Allow Outputs
+                conn.setUseCaches(false); // Don't use a Cached Copy
+                conn.setRequestMethod("POST");
+                conn.setRequestProperty("Connection", "Keep-Alive");
+                conn.setRequestProperty("ENCTYPE", "multipart/form-data");
+                conn.setRequestProperty("Content-Type", "multipart/form-data;boundary=" + boundary);
+                conn.setRequestProperty("uploaded_file", fileName);
+
+                dos = new DataOutputStream(conn.getOutputStream());
+
+                dos.writeBytes(twoHyphens + boundary + lineEnd);
+                dos.writeBytes("Content-Disposition: form-data; name=\"uploaded_file\";filename=\""
+                        + fileName + "\"" + lineEnd);
+
+                dos.writeBytes(lineEnd);
+
+                // create a buffer of  maximum size
+                bytesAvailable = fileInputStream.available();
+
+                bufferSize = Math.min(bytesAvailable, maxBufferSize);
+                buffer = new byte[bufferSize];
+
+                // read file and write it into form...
+                bytesRead = fileInputStream.read(buffer, 0, bufferSize);
+
+                while (bytesRead > 0) {
+
+                    dos.write(buffer, 0, bufferSize);
+                    bytesAvailable = fileInputStream.available();
+                    bufferSize = Math.min(bytesAvailable, maxBufferSize);
+                    bytesRead = fileInputStream.read(buffer, 0, bufferSize);
+
+                }
+
+                // send multipart form data necesssary after file data...
+                dos.writeBytes(lineEnd);
+                dos.writeBytes(twoHyphens + boundary + twoHyphens + lineEnd);
+
+                // Responses from the server (code and message)
+                serverResponseCode = conn.getResponseCode();
+                String serverResponseMessage = conn.getResponseMessage();
+
+                Log.i("uploadFile", "HTTP Response2 is : "
+                        + serverResponseMessage + ": " + serverResponseCode);
+
+                if(serverResponseCode == 200){
+
+                    runOnUiThread(new Runnable() {
+                        public void run() {
+
+                            String msg = "File Upload Completed.\n\n See uploaded file here : \n\n"
+                                    +"http://impact.asu.edu/CSE535Spring18Folder/"
+                                    +uploadFileName;
+
+                            Toast.makeText(MainActivity.this, "File Upload Complete.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }
+
+                //close the streams //
+                fileInputStream.close();
+                dos.flush();
+                dos.close();
+
+            } catch (MalformedURLException ex) {
+
+                dialog.dismiss();
+                ex.printStackTrace();
+
+                runOnUiThread(new Runnable() {
+                    public void run() {
+                        Toast.makeText(MainActivity.this, "MalformedURLException",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+                Log.e("Upload file to server", "error: " + ex.getMessage(), ex);
+            } catch (Exception e) {
+
+                dialog.dismiss();
+                e.printStackTrace();
+
+                runOnUiThread(new Runnable() {
+                    public void run() {
+                        Toast.makeText(MainActivity.this, "Got Exception : see logcat ",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                });
+                Log.e("Upload file to server Exception", "Exception : "
+                        + e.getMessage(), e);
+            }
+            dialog.dismiss();
+            return;
+
+        }
     }
 
 }
